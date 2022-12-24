4fd0ae0eebff32967483558084700f86f6922205
rushikesh <rushikeshkarale72@gmail.com>
2022-12-24 18:04:23 +0530

    ok

---
+++ assignment2/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import main from './components/main';
+
+function App() {
+  return (
+    <div className="App">
+      <main/>
+    </div>
+  );
+}
+
+export default App;
---
+++ assignment3/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <br/>
+     <h1>The Evolution of <br/> Airbnbs frontend </h1>
+     <br/>
+     <br/>
+     <br/>
+     <h5>@spikebrehm</h5>
+     <br/>
+     <br/>
+     <p>airbub</p>
+     <br/>
+    </div>
+  );
+}
+
+export default App;
