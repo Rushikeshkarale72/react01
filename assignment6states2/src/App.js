5f08cb1199dabe310922217d2b49b31ac4e018ab
Rushikeshkarale <99477684+Rushikeshkarale72@users.noreply.github.com>
2022-12-30 14:26:03 +0530

    ok

--- assignment6states2/src/App.js
+++ assignment6states2/src/App.js
@@ -1,8 +1,15 @@
 
-import './App.css';
-import Markdown from './components/Markdown';
+import "./App.css";
+import Cheetsheet from "./components/Cheetsheet";
+import Editor from "./components/Editor";
+
 function App() {
-  return <Markdown />;
+  return (
+    <div>
+      <Cheetsheet />
+      <Editor />
+    </div>
+  );
 }
 
 export default App;
