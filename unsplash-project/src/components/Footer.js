3c21ddee4e3c47da6c52dff017777fb736c462f0
Rushikeshkarale <99477684+Rushikeshkarale72@users.noreply.github.com>
2023-01-16 16:32:05 +0530

    ok

---
+++ unsplash-project/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-light footer">
+      <p>Developed By Rushikesh Karale!</p>
+    </div>
+  );
+};
+
+export default Footer;
